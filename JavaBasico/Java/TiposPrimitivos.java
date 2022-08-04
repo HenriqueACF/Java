acdafcf2c4857b8c7e6f1bcf757e6cd62240d098
henriqueacf <henrique.assis145@gmail.com>
2022-08-04 15:42:34 -0300

    tipos primitivos

---
+++ JavaBasico/Java/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package JavaBasico.Java;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //Inteiros
+        byte idade = 25;
+        short idade2 = 25;
+        int idade3 = 25;
+        long idade4 = 25;
+
+        //Pontos flutuantes
+        double valor = 2.50;
+        float valor2 = 2.50f;
+
+        //caractere
+        char o = 'o';
+
+        //boleanos
+        boolean verdadeiro = true;
+        boolean falso = false;
+    }
+}
