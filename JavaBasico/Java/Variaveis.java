ee31c6a4cd8b875f070808fc2b60cf3d52129b13
henriqueacf <henrique.assis145@gmail.com>
2022-08-04 15:20:53 -0300

    variaveis

---
+++ JavaBasico/Java/Variaveis.java
@@ -0,0 +1,10 @@
+package JavaBasico.Java;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade;
+        idade = 25;
+        String nome = "Henrique";
+        System.out.println("idade: " + idade);
+    }
+}
