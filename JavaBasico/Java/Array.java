dc48adc22fc3da445034878c70ff723cffdd855a
henriqueacf <henrique.assis145@gmail.com>
2022-08-05 10:50:29 -0300

    vetores

--- JavaBasico/Java/Array.java
+++ JavaBasico/Java/Array.java
@@ -12,9 +12,21 @@ public class Array {
         System.out.println("o valor da temperatura do dia 2 é: " + temperaturas[1]);
         System.out.println("o tamanho do array é: " + temperaturas.length);
 
-        // foreach
+        // for para array
         for (double temp: temperaturas ) {
             System.out.println(temp);
         }
+
+        // matriz
+        double [][] notasAlunos = new double[30][4];
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7.5;
+        notasAlunos[0][2] = 6.8;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 7;
+        notasAlunos[1][1] = 7.5;
+        notasAlunos[1][2] = 8.8;
+        notasAlunos[1][3] = 7.5;
     }
 }
