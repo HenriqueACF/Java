512582377068327d67d4ea260e75054fa7b92c0e
henriqueacf <henrique.assis145@gmail.com>
2022-08-05 16:29:52 -0300

    static

--- JavaPOO/MinhaCalculadora.java
+++ JavaPOO/MinhaCalculadora.java
@@ -2,15 +2,15 @@ package JavaPOO;
 
 public class MinhaCalculadora {
 
-    public int soma(int num1, int num2){
+    public static int soma(int num1, int num2){
         return num1 + num2;
     }
 
-    public double soma(double num1, double num2){
+    public static double soma(double num1, double num2){
         return num1 + num2;
     }
 
-    public int soma(int num1, int num2, int num3){
+    public static int soma(int num1, int num2, int num3){
         return num1 + num2 + num3;
     }
 }
--- JavaPOO/TesteCalculadora.java
+++ JavaPOO/TesteCalculadora.java
@@ -2,8 +2,10 @@ package JavaPOO;
 
 public class TesteCalculadora {
     public static void main(String[] args) {
-        MinhaCalculadora calc = new MinhaCalculadora();
-
-        calc.soma(1,2);
+//        MinhaCalculadora calc = new MinhaCalculadora();
+//
+//        calc.soma(1,2);
+        int resultado = MinhaCalculadora.soma(1,2,3);
+        System.out.println(resultado);
     }
 }
