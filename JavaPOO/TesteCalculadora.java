1cc80116c3bd2ab252e7885372ad451e252f4be2
henriqueacf <henrique.assis145@gmail.com>
2022-08-05 16:19:40 -0300

    sobrecarga de metodos

---
+++ JavaPOO/TesteCalculadora.java
@@ -0,0 +1,9 @@
+package JavaPOO;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        MinhaCalculadora calc = new MinhaCalculadora();
+
+        calc.soma(1,2);
+    }
+}
