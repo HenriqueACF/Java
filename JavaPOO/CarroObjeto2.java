fd17f7dc62c0388deaf387e362510d4cf4c664d2
henriqueacf <henrique.assis145@gmail.com>
2022-08-05 16:08:00 -0300

    metodos get e set

---
+++ JavaPOO/CarroObjeto2.java
@@ -0,0 +1,52 @@
+package JavaPOO;
+
+public class CarroObjeto2 {
+
+    private String marca;
+    private String modelo;
+    private int numPassageiros;
+    private double capCombustivel;
+    private double consumoCombustivel;
+
+
+    //metodos GET e SET
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNumPassageiros() {
+        return numPassageiros;
+    }
+
+    public void setNumPassageiros(int numPassageiros) {
+        this.numPassageiros = numPassageiros;
+    }
+
+    public double getCapCombustivel() {
+        return capCombustivel;
+    }
+
+    public void setCapCombustivel(double capCombustivel) {
+        this.capCombustivel = capCombustivel;
+    }
+
+    public double getConsumoCombustivel() {
+        return consumoCombustivel;
+    }
+
+    public void setConsumoCombustivel(double consumoCombustivel) {
+        this.consumoCombustivel = consumoCombustivel;
+    }
+}
---
+++ JavaPOO/CarroTeste2.java
@@ -0,0 +1,10 @@
+package JavaPOO;
+
+public class CarroTeste2 {
+    public static void main(String[] args) {
+
+        CarroObjeto2 van = new CarroObjeto2();
+        van.setMarca("fiat");
+        System.out.println(van.getMarca());
+    }
+}
