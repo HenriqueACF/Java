80e3d62d824825828ac1c3f152350f1c3c212d91
henriqueacf <henrique.assis145@gmail.com>
2022-08-05 11:27:16 -0300

    iniciando poo -> objetos

---
+++ JavaPOO/CarroObjeto.java
@@ -0,0 +1,10 @@
+package JavaPOO;
+
+public class CarroObjeto {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombistivel;
+    double consumoCombustivel;
+}
---
+++ JavaPOO/CarroTeste.java
@@ -0,0 +1,12 @@
+package JavaPOO;
+
+public class CarroTeste {
+    public static void main(String[] args) {
+        CarroObjeto van = new CarroObjeto();
+        van.marca = "fiat";
+        van.modelo = "ducato";
+        van.numPassageiros = 10;
+        van.capCombistivel= 100;
+        van.consumoCombustivel = 0.2;
+    }
+}
