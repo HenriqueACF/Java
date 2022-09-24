90012587b387442c838758211bdb8d993c321909
HenriqueACF <henrique.assis145@gmail.com>
2022-09-24 18:33:09 -0300

    init correntista

--- SistemaBancarioPOO/src/Correntista.java
+++ SistemaBancarioPOO/src/Correntista.java
@@ -1,4 +1,21 @@
 public class Correntista {
-    private int numero;
+    private int codigo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     private String nome;
+
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
 }
